cc1469afbea8d29058d779ed4154856cbeb5c0e2
bensooraj <bensooraj@users.noreply.github.com>
2024-03-22 17:11:31 +0530

    further reduced the size of the model

--- models.py
+++ models.py
@@ -8,55 +8,55 @@ class NetBN(nn.Module):
         super(NetBN, self).__init__()
         # Input Block
         self.convBlock1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU()
         )
         
         # Convolution Block 1
         self.convBlock2 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU()
         )
 
         # Transition Block 1
         self.convBlock3 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.Conv2d(in_channels=16, out_channels=8, kernel_size=1, stride=1, padding=0, bias=True),
             nn.ReLU()
         )
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # Convolution Block 2
         self.convBlock4 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU()
         )
         self.convBlock5 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU()
         )
         self.convBlock6 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU()
         )
 
         # Transition Block 2
         self.convBlock7 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=16, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.Conv2d(in_channels=64, out_channels=8, kernel_size=1, stride=1, padding=0, bias=True),
             nn.ReLU()
         )
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # Convolution Block 3
         self.convBlock8 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU()
         )
         self.convBlock9 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU()
         )
         self.convBlock10 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU()
         )
         
@@ -65,7 +65,7 @@ class NetBN(nn.Module):
 
         # Output Block
         self.convBlock11 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=10, kernel_size=1, stride=1, padding=0, bias=True)
+            nn.Conv2d(in_channels=64, out_channels=10, kernel_size=1, stride=1, padding=0, bias=True)
         )
     
     def forward(self, x):
